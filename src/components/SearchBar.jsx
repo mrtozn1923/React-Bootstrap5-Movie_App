79974247f5877913c265c581459690e7f5fd08b9
mrtozn1923 <mrtozn1923@gmail.com>
2021-06-27 15:25:19 +0300

    Created Project

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class MovieList extends Component {
+    render() {
+        return (
+            <h1>Search Bar</h1>
+        )
+    }
+}
+
+export default MovieList;
