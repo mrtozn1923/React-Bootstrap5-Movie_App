63fc435226b84b031748b42ffc463396e2f76f9d
mrtozn1923 <mrtozn1923@gmail.com>
2021-06-28 07:10:53 +0300

    react router support added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,56 +1,69 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import MovieList from './MovieList';
+import AddMovie from './AddMovie';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import axios from 'axios';
 
-class App extends React.Component{
-    
-    state={
+class App extends React.Component {
+
+    state = {
         movies: [],
-        searchQuery:""
+        searchQuery: ""
     }
 
-    async componentDidMount(){
-        const response=await axios.get("http://localhost:3004/movies");
-        this.setState({movies:response.data});
+    async componentDidMount() {
+        const response = await axios.get("http://localhost:3004/movies");
+        this.setState({ movies: response.data });
     }
-    
-    deleteMovie=async (movie)=>{
+
+    deleteMovie = async (movie) => {
         axios.delete(`http://localhost:3004/movies/${movie.id}`);
-        const newMovieList=this.state.movies.filter(
-            m=>m.id!==movie.id
+        const newMovieList = this.state.movies.filter(
+            m => m.id !== movie.id
         );
-        this.setState(()=>({
-            movies:newMovieList
+        this.setState(() => ({
+            movies: newMovieList
         }));
     }
 
-    searchMovie=(e)=>{
-        this.setState({searchQuery:e.target.value});
+    searchMovie = (e) => {
+        this.setState({ searchQuery: e.target.value });
     }
 
-    render(){
-        let filteredMovies=this.state.movies.filter(
-            (movie)=>{
-                return movie.name.toLowerCase().indexOf(this.state.searchQuery.toLowerCase())!==-1
+    render() {
+        let filteredMovies = this.state.movies.filter(
+            (movie) => {
+                return movie.name.toLowerCase().indexOf(this.state.searchQuery.toLowerCase()) !== -1
             }
         )
-        return(
-            <div className="container">
-                <div className="row">
-                    <h1 className="text-center mt-3">REACTJS - MOVIE APP</h1>
+        return (
+            <Router>
+                <div className="container">
+                    <Switch>
+                        <Route path="/" exact render={() => (
+                            <React.Fragment>
+                                <div className="row">
+                                    <h1 className="text-center mt-3">REACTJS - MOVIE APP</h1>
+                                </div>
+                                <div className="row">
+                                    <div className="col-lg-12">
+                                        <SearchBar searchMovie={this.searchMovie} />
+                                    </div>
+                                </div>
+
+                                <MovieList
+                                    movies={filteredMovies}
+                                    deleteMovie={this.deleteMovie}
+                                />
+                            </React.Fragment>
+                        )}>
+                        </Route>
+                        <Route path="/add" component={AddMovie} />
+                    </Switch>
                 </div>
-                <div className="row">
-                    <div className="col-lg-12">
-                        <SearchBar searchMovie={this.searchMovie}/>
-                    </div>
-                </div>
-                <MovieList 
-                    movies={filteredMovies}
-                    deleteMovie={this.deleteMovie}
-                />
-            </div>
+            </Router>
         )
-    }  
+    }
 }
 export default App;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,19 +2,24 @@ import React, { Component } from 'react'
 
 class SearchBar extends Component {
 
-    handleFormSubmit=(e)=>{
+    handleFormSubmit = (e) => {
         e.preventDefault();
         console.log(this.state.searchQuery);
     }
     render() {
         return (
-           <form onSubmit={this.handleFormSubmit}>
-               <div className="form-row my-5">
-                   <div className="col-12">
-                       <input onChange={this.props.searchMovie} type="text" className="form-control" placeholder="Search a movie"/>
-                   </div>
-               </div>
-           </form>
+            <form onSubmit={this.handleFormSubmit}>
+                <div className="row my-5">
+                    <div className="col-10">
+                         <input onChange={this.props.searchMovie} type="text" className="form-control " placeholder="Search a movie" />
+                    </div>
+                    <div className="col-2">
+                        <button type="button" className="btn btn-primary">
+                            Add Movie
+                        </button>
+                    </div>
+                </div>
+            </form>
         )
     }
 }
